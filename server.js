049e52d1f06ad2aa23c8021cfa661a2179ba1372
unknown <dekunbifola100@gmail.com>
2021-02-13 18:11:03 +0100

    Initial commit

--- server.js
+++ server.js
@@ -1,16 +1,24 @@
+require('dotenv').config()
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
+var multer = require("multer");
+
+var storage = multer.memoryStorage();
+var upload = multer({ storage: storage });
 
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
+  const { originalname, size, mimetype } = req.file;
+  res.status(200).json({ name: originalname, type: mimetype, size: size });
+});
+
 app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
-
 
 
 
